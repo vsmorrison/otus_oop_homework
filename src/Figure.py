d77d2315f0e09e910b930b54ccfdde4d1327520c
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-03-31 00:23:33 +0300

    Remove odd attributes

--- src/Figure.py
+++ src/Figure.py
@@ -1,9 +1,7 @@
 class Figure:
 
-    def __init__(self, name, area, perimeter):
-        self.name = name
+    def __init__(self, area):
         self.area = area
-        self.perimeter = perimeter
 
     def add_area(self, shape):
         if not isinstance(shape, Figure):
